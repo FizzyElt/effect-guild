0b48ebc93c97a968cec12a9ba0c549da327fd1c1
FizzyElt <fizzyelt.dev@outlook.com>
2025-06-17 11:53:37 +0800

    update

--- examples/context.ts
+++ examples/context.ts
@@ -1,26 +1,19 @@
 import { Context, Effect, pipe } from "effect";
 
 class EnvContext extends Context.Tag("EnvContext")<
-	EnvContext,
-	{
-		apiKey: string;
-	}
->() {}
-
-class EnvContext extends Context.Tag("EnvContext")<
-	EnvContext,
-	{
-		apiKey: string;
-	}
+  EnvContext,
+  {
+    apiKey: string;
+  }
 >() {}
 
 const program = pipe(
-	EnvContext,
-	Effect.flatMap(({ apiKey }) => Effect.succeed(apiKey)),
+  EnvContext,
+  Effect.flatMap(({ apiKey }) => Effect.succeed(apiKey)),
 );
 
 const providedProgram = program.pipe(
-	Effect.provideService(EnvContext, { apiKey: "123" }),
+  Effect.provideService(EnvContext, { apiKey: "123" }),
 );
 
 Effect.runPromise(providedProgram).then(console.log);
--- examples/function.ts
+++ examples/function.ts
@@ -1,34 +1,29 @@
-import { Array, String, pipe, Match, Either } from "effect";
+import { Array, String, pipe, Match } from "effect";
 
 // 12345678 => 1,234,567
 const formatNumberWithCommas = (n: number) =>
-	pipe(
-		n.toString(),
-		String.split(""),
-		Array.reverse,
-		Array.chunksOf(3),
-		Array.map((chunk) => pipe(chunk, Array.reverse, Array.join(""))),
-		Array.reverse,
-		Array.join(","),
-	);
+  pipe(
+    n.toString(),
+    String.split(""),
+    Array.reverse,
+    Array.chunksOf(3),
+    Array.map((chunk) => pipe(chunk, Array.reverse, Array.join(""))),
+    Array.reverse,
+    Array.join(","),
+  );
 
 console.log(formatNumberWithCommas(1234567));
 
-type Hex = {
-	type: "hex";
-	code: string;
-};
-
 type Rgb = {
-	r: number;
-	g: number;
-	b: number;
+  r: number;
+  g: number;
+  b: number;
 };
 
 const rgbToName = Match.type<Rgb>().pipe(
-	Match.withReturnType<string>(),
-	Match.when({ r: 255, g: 0, b: 0 }, () => "red"),
-	Match.when({ r: 0, g: 0, b: 255 }, () => "blue"),
-	Match.when({ r: 0, g: 255, b: 0 }, () => "green"),
-	Match.orElse(() => "unknown"),
+  Match.withReturnType<string>(),
+  Match.when({ r: 255, g: 0, b: 0 }, () => "red"),
+  Match.when({ r: 0, g: 0, b: 255 }, () => "blue"),
+  Match.when({ r: 0, g: 255, b: 0 }, () => "green"),
+  Match.orElse(() => "unknown"),
 );
